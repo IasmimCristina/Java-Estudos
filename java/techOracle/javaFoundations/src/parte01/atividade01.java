3225de2d691acd9f6f6b2b2497b56b43a9282753
Iasmim Pereira <iaasmimcristinaa@gmail.com>
2022-04-21 20:47:38 -0300

    Atividade em continuidade

---
+++ java/techOracle/javaFoundations/src/parte01/atividade01.java
@@ -0,0 +1,22 @@
+package parte01;
+
+public class atividade01 {
+    public static void main(String[] args) {
+        System.out.println("Há um gato abaixo!");
+    
+
+    
+        System.out.println("   /\\         /\\   ");  System.out.println("  /  \\_______/  \\  ");      System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+
+        System.out.println("Miau!");
+        
+    }
+
+
+    
+}
