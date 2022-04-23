480fc85f249d4cf2ed59f6951c4ec4165d075989
Iasmim Pereira <iaasmimcristinaa@gmail.com>
2022-04-23 12:24:36 -0300

    Término de atividade

---
+++ java/techOracle/javaFoundations/src/parte01/Atividade02.java
@@ -0,0 +1,12 @@
+package parte01;
+
+public class Atividade02 {
+
+    public static void main(String[] args) {
+        int x = 25 - 5 * 4 / 2 - 10 + 4;
+        System.out.println(x);
+        
+    }
+
+    
+}
