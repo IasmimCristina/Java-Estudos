3897288a40806fa022c34226368378274412121d
Iasmim Pereira <iaasmimcristinaa@gmail.com>
2022-05-10 13:40:27 -0300

    Projeto aberto, ainda não resolvido.

---
+++ java/unifacs/abstracaoPoo/src/AbstracaoPOO/Main.java
@@ -0,0 +1,11 @@
+package AbstracaoPOO;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Os métodos abstratos são apenas uma assinatura na classe mãe. As classes filhas têm essa responsabilidade.
+    //O polimorfismo permite que classes abstratas consigam receber comportamentos através de classes concertas.
+        System.out.println("Exemplo!");
+
+    }
+}
