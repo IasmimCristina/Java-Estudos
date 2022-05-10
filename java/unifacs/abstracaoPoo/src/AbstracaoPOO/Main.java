20b609ab53ddb52a0bb536abe2e6b28514796757
Iasmim Pereira <iaasmimcristinaa@gmail.com>
2022-05-10 13:46:20 -0300

    Início da prática com classes e métodos abstratos.

--- java/unifacs/abstracaoPoo/src/AbstracaoPOO/Main.java
+++ java/unifacs/abstracaoPoo/src/AbstracaoPOO/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// Os métodos abstratos são apenas uma assinatura na classe mãe. As classes filhas têm essa responsabilidade.
-    //O polimorfismo permite que classes abstratas consigam receber comportamentos através de classes concertas.
+    //O polimorfismo permite que classes abstratas consigam receber comportamentos através de classes concretas.
         System.out.println("Exemplo!");
 
     }
